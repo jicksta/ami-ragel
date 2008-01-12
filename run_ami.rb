111503ec45beea470f7028b1905c506bbe37f329
Jay Phillips <jicksta+git@gmail.com>
2008-01-11 16:55:02 -0800

    Fixed the damn broken state machine. Working nicely now

--- run_ami.rb
+++ run_ami.rb
@@ -3,9 +3,10 @@ require File.join(File.dirname(__FILE__), 'ami.rb')
 parser = RagelGeneratedAMIProtocolStateMachine.new
 
 packets = ["Asterisk Call Manager/1.0\r\n",
-           "Response: Success\r\nMessage: Authentication accepted\r\n",
-           "Response: Pong\r\n",
-           "Response: Pong\r\nActionID: 1337\r\n"]
+           "Response: Success\r\nMessage: Authentication accepted\r\n\r\n",
+           "Response: Pong\r\n\r\n",
+           "Response: Pong\r\nActionID: 1337\r\n\r\n",
+           "Event: Registry\r\nPrivilege: system,all\r\nChannelDriver: SIP\r\nDomain: proxy.voip.ms\r\nStatus: Registered\r\n\r\n"]
 
 packets.each do |packet|
   parser << packet
